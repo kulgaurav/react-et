7ba71a6bb795731d570f7e8fabbb75ac1bc882f8
warkulock <kulgaurav23@gmail.com>
2020-02-04 21:08:47 +0000

    Elements

---
+++ Ch02/02_02/start/react-is-fun/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var style = {
+    backgroundColor: 'Orange',
+    color: 'white'
+}
+
+
+
+
+ReactDOM.render(
+    <h1 style={style}>Heyyyy</h1>,
+    document.getElementById('root')
+)
